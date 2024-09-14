15a7887965743f362f97d5805271be33f8629e32
sandro <sbuzhghulashvili@gmail.com>
2024-09-15 01:00:20 +0400

    added login and logout requests

--- back-end/controllers/Client/clientValidation.js
+++ back-end/controllers/Client/clientValidation.js
@@ -1,30 +1,30 @@
-import { z } from 'zod';
-import bcrypt from 'bcrypt';
+import { z } from "zod";
+import bcrypt from "bcrypt";
 
 const clientSchema = z
   .object({
-    name: z.string().min(1, { message: 'Please type your name' }),
-    email: z.string().email({ message: 'Invalid email address' }),
+    name: z.string().min(1, { message: "Please type your name" }),
+    email: z.string().email({ message: "Invalid email address" }),
     phoneNumberExtension: z
       .string()
-      .length(4, { message: 'Type a correct extension' }),
+      .length(4, { message: "Type a correct extension" }),
     phoneNumber: z
       .string()
-      .regex(/^\d{9}$/, { message: 'Phone number must be 9 digits' }),
+      .regex(/^\d{9}$/, { message: "Phone number must be 9 digits" }),
     password: z
       .string()
-      .min(7, { message: 'Password must be at least 7 characters' }),
+      .min(7, { message: "Password must be at least 7 characters" }),
     confirmPassword: z
       .string()
-      .min(7, { message: 'Confirm password must be at least 7 characters' }),
+      .min(7, { message: "Confirm password must be at least 7 characters" }),
   })
   .refine((data) => data.password === data.confirmPassword, {
     message: "Passwords don't match",
-    path: ['confirmPassword'],
+    path: ["confirmPassword"],
   })
-  .refine((data) => data.phoneNumberExtension[0] === '+', {
-    message: 'Type a correct extension',
-    path: ['phoneNumberExtension'],
+  .refine((data) => data.phoneNumberExtension[0] === "+", {
+    message: "Please Include + in extenstion",
+    path: ["phoneNumberExtension"],
   });
 
 export const validateClient = (data) => {
@@ -40,7 +40,23 @@ export const validateClient = (data) => {
       },
     };
   } catch (error) {
-    return { success: false, error: error.errors };
+    let errors = null;
+
+    if (error.errors.some((err) => err.message === "Required")) {
+      return { success: false, error: "Please fill the form" };
+    }
+
+    if (error.errors.length > 1) {
+      errors = error.errors.reduce((start, err) => {
+        return start + `${err.message}, `;
+      }, "");
+    }
+
+    if (error.errors.length === 1) {
+      errors = error.errors[0].message;
+    }
+
+    return { success: false, error: errors };
   }
 };
 
@@ -52,6 +68,6 @@ export const hashPassword = async (password) => {
     const hashedPassword = await bcrypt.hash(password, salt);
     return hashedPassword;
   } catch (error) {
-    throw new Error('Error hashing password');
+    throw new Error("Error hashing password");
   }
 };
