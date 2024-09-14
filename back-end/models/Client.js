7a89bf57731522c315cff2478719c6a26a182aca
Sandro <sbuzhghulashvili@gmail.com>
2024-09-14 14:48:38 +0400

    client sign up - post req

--- back-end/models/Client.js
+++ back-end/models/Client.js
@@ -1,25 +1,40 @@
 import mongoose from 'mongoose';
 const Schema = mongoose.Schema;
 
-const clientSchema = new Schema({
-  username: {
-    type: String,
-    required: true,
-    unique: true,
+const clientSchema = new Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    email: {
+      type: String,
+      required: true,
+    },
+    phoneNumber: {
+      type: String,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    lastLogin: {
+      type: Date,
+      default: null,
+    },
+    isVerified: {
+      type: Boolean,
+      default: false,
+    },
+    resetPasswordToken: String,
+    resetPasswordExpiresAt: Date,
+    verificationToken: String,
+    verificationTokenExpiresAt: Date,
   },
-  email: {
-    type: String,
-    required: true,
-  },
-  phoneNumber: {
-    type: String,
-    required: true,
-  },
-  password: {
-    type: String,
-    required: true,
-  },
-});
+  { timestamps: true } // by that we get createdAt and updatedAt props into document
+);
 
 const Client = mongoose.model('Client', clientSchema);
 
