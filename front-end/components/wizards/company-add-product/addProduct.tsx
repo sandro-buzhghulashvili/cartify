77f40675800fdeb4aa3e42e2293c6b4a9e09a8b8
sandro <sbuzhghulashvili@gmail.com>
2024-11-03 17:01:15 +0400

    hot fix

--- front-end/components/wizards/company-add-product/addProduct.tsx
+++ front-end/components/wizards/company-add-product/addProduct.tsx
@@ -1,22 +1,29 @@
-import { colorValidation } from '@/utils/validateColor';
-import AboutProduct from './AboutProduct';
-import FinishProduct from './FinishProduct';
-import MainTypes from './MainTypes';
-import ProductImages from './ProductImages';
+import { colorValidation } from "@/utils/validateColor";
+import AboutProduct from "./AboutProduct";
+import FinishProduct from "./FinishProduct";
+import MainTypes from "./MainTypes";
+import ProductImages from "./ProductImages";
 
 export const addProductsWizardsData = [
   {
-    title: 'about_company',
+    title: "about_company",
     component: <AboutProduct />,
     answer: null,
     validationFn: (data: any) =>
       data &&
       data.title.trim().length > 0 &&
       data.description.trim().length > 50,
-    errorMessage: 'Please provide title and description',
+    errorMessage: "Please provide title and description",
   },
   {
-    title: 'main_types',
+    title: "images",
+    component: <ProductImages />,
+    answer: null,
+    validationFn: () => {},
+    errorMessage: "Please provide images",
+  },
+  {
+    title: "main_types",
     component: <MainTypes />,
     answer: null,
     validationFn: (data: any) => {
@@ -25,7 +32,7 @@ export const addProductsWizardsData = [
       const areTypesValid =
         Array.isArray(data.types) &&
         data.types.every(
-          ({ val }: any) => typeof val === 'string' && val.trim().length > 0
+          ({ val }: any) => typeof val === "string" && val.trim().length > 0
         );
 
       // Validate colors array
@@ -41,17 +48,10 @@ export const addProductsWizardsData = [
 
       return areTypesValid && areColorsValid && isStockValid;
     },
-    errorMessage: 'Please provide all neccessary features',
+    errorMessage: "Please provide all neccessary features",
   },
   {
-    title: 'images',
-    component: <ProductImages />,
-    answer: null,
-    validationFn: () => {},
-    errorMessage: 'Please provide images',
-  },
-  {
-    title: 'finish',
+    title: "finish",
     component: <FinishProduct />,
   },
 ];
